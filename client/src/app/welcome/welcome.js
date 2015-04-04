120ec447c0cd7a2d606094ac142aeec4757ae3af
Evgeny Demidchenko <promocoding@gmail.com>
2015-04-05 00:17:12 +0300

    Create Welcome Page controller. Delete Home-Controller

---
+++ client/src/app/welcome/welcome.js
@@ -0,0 +1,33 @@
+define(['angular'], function(angular) {
+    'use strict';
+
+    /**
+     * @module Welcome
+     */
+  angular.module('reviewApp.welcome', ['ui.router'])
+
+    /**
+     * @module Welcome
+     * @class Route
+     */
+    .config(['$stateProvider', function config($stateProvider) {
+      $stateProvider.state('welcome', {
+        url: '/welcome',
+        views: {
+          main: {
+            controller: 'WelcomeCtrl',
+            templateUrl: 'app/welcome/welcome.tpl.html'
+          }
+        }
+      });
+    }])
+
+    /**
+     * @module Welcome
+     * @class WelcomeCtrl (controller)
+     */
+    .controller('WelcomeCtrl', function WelcomeCtrl($scope) {
+        $scope.message = 'Welcome to the Site!';
+    });
+  }
+);
