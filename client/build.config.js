b760ba90fa056b87f350ee9c7b44a7afda1f7ed5
Evgeny Demidchenko <promocoding@gmail.com>
2015-04-06 16:41:43 +0300

    Fix karma config

--- client/build.config.js
+++ client/build.config.js
@@ -4,7 +4,7 @@ module.exports = {
 
   app_files: {
     js: ['src/**/*.js', '!src/**/*.spec.js', '!src/**/*.scenario.js'],
-    jsunit: ['src/**/*.spec.js'],
+    jsunit: 'src/**/*.spec.js',
     jsscenario: ['src/**/*.scenario.js'],
     html: ['src/index.html'],
     styles: ['assets/*.css'],
