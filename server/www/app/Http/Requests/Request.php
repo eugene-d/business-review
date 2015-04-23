c2fe9b5c83f98f0ca965e74522fc3d3e19ce6464
Nik <nikbox6@gmail.com>
2015-04-23 11:24:19 +0300

    Change response for Request validations.

--- server/www/app/Http/Requests/Request.php
+++ server/www/app/Http/Requests/Request.php
@@ -1,9 +1,18 @@
 <?php namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\JsonResponse;
+
 
 abstract class Request extends FormRequest {
-
-	//
-
+    /**
+     * override Illuminate\Foundation\Http\FormRequest
+     * Custom realization of response method to avoid redirect and always return JSON response object
+     * @param array $errors
+     * @return $this|JsonResponse
+     */
+    public function response(array $errors) {
+        $statusCode = 422;
+        return new JsonResponse(['status' => $statusCode, 'errors' => $errors], $statusCode);
+    }
 }
