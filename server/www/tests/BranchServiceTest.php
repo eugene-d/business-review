8af0b72f3a214c77ee4b2f88614cf10355290568
Nik <nikbox6@gmail.com>
2015-04-24 20:46:04 +0300

    Add test for branch updating service.

--- server/www/tests/BranchServiceTest.php
+++ server/www/tests/BranchServiceTest.php
@@ -12,9 +12,14 @@ class BranchServiceTest extends TestCase {
     private $descriptions;
     private $locations;
     private $categories;
+    private $attributes = ['email' => 'emails', 'site' => 'sites', 'phone' => 'phones',
+        'description_us' => 'descriptions', 'city_id' => 'locations', 'category_id' => 'categories', 'id' => 1];
 
-    public function __construct() {
-        $this->branches = Mockery::mock('App\Models\Branches')->makePartial();
+    public function setUp() {
+        parent::setUp();
+
+        $this->branches = Mockery::mock('Eloquent', 'App\Models\Branches');
+        $this->createApplication()->instance('App\Models\Branches', $this->branches);
 
         $this->emails = Mockery::mock('App\Models\Branches\Emails');
         $this->sites = Mockery::mock('App\Models\Branches\Sites');
@@ -27,28 +32,61 @@ class BranchServiceTest extends TestCase {
             $this->locations, $this->categories);
     }
 
+    public function tearDown() {
+        Mockery::close();
+    }
+
     /**
      * Test branch creation with all relative records
      */
     public function testCreateWithAllRelatives() {
-        $attributes = ['email' => 'emails', 'site' => 'sites', 'phone' => 'phones',
-            'description_us' => 'descriptions', 'city_id' => 'locations', 'category_id' => 'categories'];
+        $this->branches->makePartial()->shouldReceive('create')->once()->with($this->attributes)
+            ->andReturn($this->branches);
 
-        $this->branches->shouldReceive('create')->once()->with($attributes)->andReturn($this->branches);
-        $this->proceedCreateMocks($attributes);
-        $this->branchService->create($attributes, $this->branches);
+        $this->proceedCreateMocks($this->attributes);
+        $this->branchService->create($this->attributes, $this->branches);
+    }
+
+    /**
+     * Test branch update with all relative records
+     */
+    public function testUpdateWithAllRelatives() {
+        $this->branches->shouldReceive('find')->once()->with(1)->andReturn($this->branches)
+            ->shouldReceive('fill')->once()->with($this->attributes)->andReturn($this->branches)
+            ->shouldReceive('push')->once()->andReturn($this->branches);
+
+        $this->setIrrelevantMethods(['getAttribute', 'hasGetMutator', 'email', 'hasOne', 'site', 'phone',
+            'description', 'location', 'category',  'save', 'newQueryWithoutScopes',
+            'getConnection', 'resolveConnection', 'getTable', 'newEloquentBuilder', 'getKeyName',
+            'setAttribute', 'hasSetMutator', 'getDates', 'syncOriginal', 'touchOwners'
+        ]);
+
+        $this->proceedCreateMocks($this->attributes);
+
+        $this->branchService->update($this->attributes, $this->branches);
     }
 
     /**
      * That method start loop processes for accepted relative models
-     * @param $attributes
      */
-    private function proceedCreateMocks($attributes){
-        foreach ($attributes as $attribute) {
-            $this->{$attribute}->shouldReceive('fill')->once()->with($attributes)->andReturn($this->{$attribute});
-            $this->{$attribute}->shouldReceive('branch')->once()->andReturn($this->{$attribute});
-            $this->{$attribute}->shouldReceive('associate')->once()->andReturn($this->{$attribute});
-            $this->{$attribute}->shouldReceive('save')->once()->andReturn($this->{$attribute});
+    private function proceedCreateMocks() {
+        foreach ($this->attributes as $attribute) {
+            if ($attribute != 1) {
+                $this->{$attribute}->shouldReceive('branch')->once()->andReturn($this->{$attribute})
+                    ->shouldReceive('fill')->once()->with($this->attributes)->andReturn($this->{$attribute})
+                    ->shouldReceive('associate')->once()->andReturn($this->{$attribute})
+                    ->shouldReceive('save')->once()->andReturn($this->{$attribute});
+            }
+        }
+    }
+
+    /**
+     * The method is setting required but not relevant methods for mock object
+     * @param $methods
+     */
+    private function setIrrelevantMethods($methods) {
+        foreach ($methods as $method) {
+            $this->branches->shouldReceive($method)->passthru();
         }
     }
 }
